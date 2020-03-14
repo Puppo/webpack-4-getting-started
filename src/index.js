72fcb5b3b011b883f2f466a996dc0275acca2377
Luca Del Puppo <luca.delpuppo@aton.com>
2020-03-14 15:08:46 +0100

    multiple bundle

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import './style.css';
 
+import getProfile from './profile';
+
 function draw() {
     const el = document.createElement('div');
     el.innerHTML = 'Hello WebPack';
     el.classList.add('hello');
+
+    const btn = document.createElement('button');
+    btn.innerHTML = 'GET PROFILE';
+    btn.onclick = getProfile;
+    el.append(btn);
+
     return el;
 }
 
